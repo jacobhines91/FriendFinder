1ef64ffe41e94a5a6e176f4ad6b8c93feac9fb96
Jacob Hines <jacob_hines@hotmail.com>
2019-10-22 20:00:23 -0500

    completed

---
+++ server.js
@@ -0,0 +1,18 @@
+
+
+var express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 8081;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
